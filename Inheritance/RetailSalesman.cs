8204495b47b72f790e592a29286c0a71ab8c4bac
anesuy <aneoden@gmail.com>
2023-01-08 16:53:51 -0400

    abstract class

---
+++ Inheritance/RetailSalesman.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_beginner_course
+{
+    public class RetailSalesman : Salesman
+    {
+        //base: it tell yuo're using the classes constructor
+        //it allows the abstract class to set those fields meaning
+        // any other class using the salesman class (base class) can have access to those fields as well without having to repeat the same code inside of their own class
+        public RetailSalesman(string firstName, string lastName) : base(firstName, lastName)
+        {
+        }
+
+        public override void Sell()
+        {
+            Console.WriteLine(string.Format("{0}, RetailSalesPerson2", this.FullName));
+        }
+    }
+}
