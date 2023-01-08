1262142c15459a7cce8bb48e88ada1d3c8473f31
anesuy <aneoden@gmail.com>
2023-01-08 02:07:18 -0400

    inheritance first commit

---
+++ Inheritance/CarSalesman.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_beginner_course
+{
+    public class CarSalesman
+    {
+        private string _firstName;
+        private string _lastName;
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", this._firstName, this._lastName);
+            }
+        }
+
+        //this is the constructor. it has the same name as the class
+        public CarSalesman (string firstName, string lastName) 
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+        }
+
+        public void Sell()
+        {
+            Console.WriteLine(String.Format("{0}, CarSalesman", this.FullName));
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace OOP_beginner_course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarSalesman stevetheSalesman = new CarSalesman("Steve", "Rogers");
+            Console.WriteLine(stevetheSalesman.FullName);
+            stevetheSalesman.Sell();
+
+
+        }
+    }
+}
