38b3de907e500f74144c8d209215983741daf16e
anesuy <aneoden@gmail.com>
2023-01-10 01:01:01 -0400

    static polymorphism

--- polymorphism/Developer.cs
+++ polymorphism/Developer.cs
@@ -9,19 +9,55 @@ namespace OOP_beginner_course
     public abstract class Developer
     {
         private string _mainDevLanguage;
-        
+
         //this is the constructor. it has the same name as the class
         public Developer(string mainDevLanguage)
         {
             this._mainDevLanguage = mainDevLanguage;
-            
+
         }
 
-        //declaring abstract here because it's the method that differs alone from the two concrete classes case
+        //    public virtual void Code(string firstExtraDevLanguage = null, string secondExtraDevLanguage = null)
+        //    {
+        //        if (firstExtraDevLanguage == null && secondExtraDevLanguage == null)
+        //        {
+        //            Console.WriteLine(string.Format("I'm coding in my favorite programming language {0}", this._mainDevLanguage));
+        //        }
+
+        //        else if (firstExtraDevLanguage != null && secondExtraDevLanguage != null)
+        //        {
+        //            Console.WriteLine(string.Format("I'm coding in my favorite programming language {0}, and {1} and {2}", this._mainDevLanguage, firstExtraDevLanguage, secondExtraDevLanguage));
+        //        }
+
+        //        else if (firstExtraDevLanguage != null)
+        //        {
+        //            Console.WriteLine(string.Format("I'm coding in my favorite programming language {0}, and {1}", this._mainDevLanguage, firstExtraDevLanguage));
+        //        }
+
+        //        else if (secondExtraDevLanguage != null)
+        //        {
+        //            Console.WriteLine(string.Format("I'm coding in my favorite programming language {0}, and {1}", this._mainDevLanguage, secondExtraDevLanguage));
+        //        }
+        //    }
+
+
+        //static polymorphism happens during compile time
+        //this way, we`re basically determining it`s different behaviour as we`re writing our code, bot while the code is running.
+        //it's much cleaner and more readable;
+
         public virtual void Code()
         {
             Console.WriteLine(string.Format("I'm coding in my favorite programming language {0}", this._mainDevLanguage));
         }
 
+        public virtual void Code(string firstExtraDevLanguage = null)
+        {
+            Console.WriteLine(string.Format("I'm coding in my favorite programming language {0}, and {1}", this._mainDevLanguage, firstExtraDevLanguage));
+        }
+
+        public virtual void Code(string firstExtraDevLanguage = null, string secondExtraDevLanguage = null)
+        {
+            Console.WriteLine(string.Format("I'm coding in my favorite programming language {0}, and {1} and {2}", this._mainDevLanguage, firstExtraDevLanguage, secondExtraDevLanguage));
+        }
     }
 }
